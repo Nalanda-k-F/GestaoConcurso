4f801c6d6a21aae97a3ac69ed6670b18f0fce117
Nalanda-KF <nalandakippertflegler@gmail.com>
2025-02-17 21:12:01 -0400

    Disciplina

--- GestaoConcurso/Controllers/DisciplinaController.cs
+++ GestaoConcurso/Controllers/DisciplinaController.cs
@@ -1,6 +1,7 @@
 ﻿using GestaoConcurso.Contexto;
 using GestaoConcurso.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GestaoConcurso.Controllers
 {
@@ -22,6 +23,20 @@ namespace GestaoConcurso.Controllers
         public async Task Salvar()
         {
             await _context.SaveChangesAsync();
+        }
+        public async Task<ActionResult<List<Disciplina>>> ListarDisciplina()
+        {
+            var disciplinas = await _context.Disciplina.ToListAsync();
+            return Ok(disciplinas);
+        }
+        public async Task<ActionResult<Disciplina>> BuscarDisciplinaId(int id)
+        {
+            var disciplina = await _context.Disciplina.FindAsync(id);
+
+            if (disciplina == null)
+                return NotFound("Disciplina não encontrado.");
+
+            return Ok(disciplina);
         }
     }
    
