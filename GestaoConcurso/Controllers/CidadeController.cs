584e736c80d460ac62a0db9ef64a710e8286069e
Leonardo Rocha <leoncrocha@gmail.com>
2025-02-17 21:14:37 -0400

    Atualizado CidadeController.cs

--- GestaoConcurso/Controllers/CidadeController.cs
+++ GestaoConcurso/Controllers/CidadeController.cs
@@ -1,6 +1,7 @@
 ﻿using GestaoConcurso.Contexto;
 using GestaoConcurso.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GestaoConcurso.Controllers
 {
@@ -22,7 +23,33 @@ namespace GestaoConcurso.Controllers
         {
             await _context.SaveChangesAsync();
         }
-        // testando
+        public async Task<ActionResult<List<Cidade>>> ListarCidades()
+        {
+            var cidade = await _context.Cidade.ToListAsync();
+            return Ok(cidade);
+        }
+        public async Task<ActionResult<Cidade>> BuscarCidadeId(int id)
+        {
+            var cidade = await _context.Candidato.FindAsync(id);
+
+            if (cidade == null)
+                return NotFound("Cidade não encontrada.");
+
+            return Ok(cidade);
+        }
+        public async Task<IActionResult> Atualizar(int id, [FromBody] Cidade cidadeAtualizado)
+        {
+            var cidade = await _context.Cidade.FindAsync(id);
+
+            if (cidade == null)
+                return NotFound("Cidade não encontrada.");
+
+            // Atualiza os campos necessários
+            cidade.Municipio = cidadeAtualizado.Municipio;
+            
+            await _context.SaveChangesAsync();
+            return Ok("Cidade atualizada com sucesso.");
+        }
     }
 }
 
