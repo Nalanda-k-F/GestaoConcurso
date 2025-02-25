50d0b4add544251aeb4c0f6fb364457e7d85dca3
Leonardo Rocha <leoncrocha@gmail.com>
2025-02-24 21:47:00 -0400

    Alteração InscriçãoController - Editar Atualizar Listar

--- GestaoConcurso/Controllers/InscricaoController.cs
+++ GestaoConcurso/Controllers/InscricaoController.cs
@@ -1,6 +1,7 @@
 ﻿using GestaoConcurso.Contexto;
 using GestaoConcurso.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GestaoConcurso.Controllers
 {
@@ -22,6 +23,42 @@ namespace GestaoConcurso.Controllers
         public async Task Salvar()
         {
             await _context.SaveChangesAsync();
+        }
+        public async Task<ActionResult<List<Inscricao>>> ListarInscricao()
+        {
+            var inscricao = await _context.Inscricao.ToListAsync();
+            return Ok(inscricao);
+        }
+        //
+        public async Task<ActionResult<Inscricao>> BuscarInscricaoId(int id)
+        {
+            var inscricao = await _context.Inscricao.FindAsync(id);
+
+            if (inscricao == null)
+                return NotFound("Inscrição não encontrada.");
+
+            return Ok(inscricao);
+        }
+        //
+        public async Task<IActionResult> EditarInscricao(int id)
+        {
+            var inscricao = await _context.Inscricao.FindAsync(id);
+            if (inscricao == null) return NotFound();
+
+            return View(inscricao);
+        }
+        public async Task<IActionResult> Atualizar(int id, [FromBody] Inscricao inscricaoAtualizado)
+        {
+            var inscricao = await _context.Candidato.FindAsync(id);
+
+            if (inscricao == null)
+                return NotFound("Inscrição não encontrada.");
+
+            // Atualiza os campos necessários
+            inscricao.DataIncricao = inscricaoAtualizado.Nome;
+            
+            await _context.SaveChangesAsync();
+            return Ok("Inscrição atualizada com sucesso.");
         }
     }
    
